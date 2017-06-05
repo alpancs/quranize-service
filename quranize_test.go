4005e707a4b34623efffc83722b664e14a7ddf9e
Alfan Nur Fauzan <alpancs@gmail.com>
2017-06-05 19:35:58 +0700

    add unit testing

---
+++ quranize_test.go
@@ -0,0 +1,37 @@
+package quranize
+
+import "testing"
+
+func TestEncodeTajri(t *testing.T) {
+	input := "tajri"
+	output := []string{"تجرع", "تجري", "تجر"}
+
+	kalimas := Encode(input)
+
+	if len(kalimas) != len(output) {
+		t.Fail()
+		return
+	}
+	for i, kalima := range kalimas {
+		if output[i] != kalima {
+			t.Fail()
+		}
+	}
+}
+
+func TestEncodeAlhamdulilah(t *testing.T) {
+	input := "alhamdulilah"
+	output := []string{"الحمد لله"}
+
+	kalimas := Encode(input)
+
+	if len(kalimas) != len(output) {
+		t.Fail()
+		return
+	}
+	for i, kalima := range kalimas {
+		if output[i] != kalima {
+			t.Fail()
+		}
+	}
+}
