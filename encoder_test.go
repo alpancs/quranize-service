6635e1c4e12feb8cd02418cf880d8ede32ea0ba4
Alfan Nur Fauzan <alpancs@gmail.com>
2017-06-14 13:01:19 +0700

    refactor

--- encoder_test.go
+++ encoder_test.go
@@ -15,19 +15,6 @@ func properlyEncoded(input string, output []string) bool {
 	return true
 }
 
-func properlyLocated(input string, output []Location) bool {
-	locations := Locate(input)
-	if len(locations) != len(output) {
-		return false
-	}
-	for i, location := range locations {
-		if output[i] != location {
-			return false
-		}
-	}
-	return true
-}
-
 func TestEncodeTajri(t *testing.T) {
 	input := "tajri"
 	output := []string{"تجري", "تأجر", "تجر", "تجار"}
@@ -81,13 +68,5 @@ func TestEncodeWatasimuFull(t *testing.T) {
 	output := []string{"واعتصموا بحبل الله"}
 	if !properlyEncoded(input, output) {
 		t.Error(Encode(input))
-	}
-}
-
-func TestLocate(t *testing.T) {
-	input := "بسم الله الرحمن الرحيم"
-	output := []Location{Location{0, 0, 0}, Location{26, 29, 19}}
-	if !properlyLocated(input, output) {
-		t.Error(Locate(input))
 	}
 }
---
+++ locater_test.go
@@ -0,0 +1,24 @@
+package quranize
+
+import "testing"
+
+func properlyLocated(input string, output []Location) bool {
+	locations := Locate(input)
+	if len(locations) != len(output) {
+		return false
+	}
+	for i, location := range locations {
+		if output[i] != location {
+			return false
+		}
+	}
+	return true
+}
+
+func TestLocate(t *testing.T) {
+	input := "بسم الله الرحمن الرحيم"
+	output := []Location{Location{0, 0, 0}, Location{26, 29, 19}}
+	if !properlyLocated(input, output) {
+		t.Error(Locate(input))
+	}
+}
