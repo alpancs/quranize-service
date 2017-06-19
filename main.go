c63259dd16bdd8b54fc12dcc69afe0f521f33fb1
Alfan Nur Fauzan <alpancs@gmail.com>
2017-06-19 23:45:58 +0700

    print all harfs

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/alpancs/quranize/route"
+	"github.com/alpancs/quranize/service"
 	"github.com/pressly/chi"
 )
 
@@ -15,7 +17,24 @@ func init() {
 	}
 }
 
+func dummy() {
+	runeSet := make(map[rune]bool)
+	for _, sura := range service.Quran.Suras {
+		for _, aya := range sura.Ayas {
+			for _, r := range []rune(aya.Text) {
+				runeSet[r] = true
+			}
+		}
+	}
+	fmt.Println(len(runeSet))
+	for r := range runeSet {
+		fmt.Printf("%c %d\n", r, r)
+	}
+}
+
 func main() {
+	dummy()
+	return
 	log.Println("Linguist is running in port " + os.Getenv("PORT"))
 	http.ListenAndServe(":"+os.Getenv("PORT"), setUpRouter())
 }
