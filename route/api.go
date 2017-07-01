bcc280ad2ae8d727dec04e40e79e83209f99e58d
Alfan Nur Fauzan <alpancs@gmail.com>
2017-07-01 14:18:25 +0700

    bug fix

--- route/api.go
+++ route/api.go
@@ -3,23 +3,16 @@ package route
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/alpancs/quranize/service"
 	"github.com/pressly/chi"
 )
 
-type (
-	Location struct {
-		Sura, Aya, Begin, End int
-		SuraName, AyaText     string
-	}
-)
-
-var (
-	simpleHarfs = []rune{' ', 'ء', 'آ', 'أ', 'ؤ', 'إ', 'ئ', 'ا', 'ب', 'ة', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ى', 'ي'}
-	quranClean  = &service.QuranClean
-	quranMin    = &service.QuranMin
-)
+type Location struct {
+	Sura, Aya, Begin, End int
+	SuraName, AyaText     string
+}
 
 func Encode(w http.ResponseWriter, r *http.Request) {
 	input := chi.URLParam(r, "input")
@@ -30,38 +23,23 @@ func Locate(w http.ResponseWriter, r *http.Request) {
 	input := chi.URLParam(r, "input")
 	locations := []Location{}
 	for _, loc := range service.Locate(input) {
-		suraName := quranMin.Suras[loc.Sura].Name
-		ayaText := quranMin.Suras[loc.Sura].Ayas[loc.Aya].Text
-		begin := offset([]rune(ayaText), loc.Index)
-		end := begin + len(input) - 2
+		suraName := service.QuranMin.Suras[loc.Sura].Name
+		ayaText := service.QuranMin.Suras[loc.Sura].Ayas[loc.Aya].Text
+		begin := indexAfterSpaces([]rune(ayaText), loc.SliceIndex)
+		end := indexAfterSpaces([]rune(ayaText), loc.SliceIndex+strings.Count(input, " ")+1) - 1
 		locations = append(locations, Location{loc.Sura, loc.Aya, begin, end, suraName, ayaText})
 	}
 	json.NewEncoder(w).Encode(locations)
 }
 
-func offset(runes []rune, n int) int {
-	for i, harf := range runes {
-		if n == 0 {
+func indexAfterSpaces(text []rune, remain int) int {
+	for i, r := range text {
+		if remain == 0 {
 			return i
 		}
-		if isSimpleHarf(harf) {
-			n--
+		if r == ' ' {
+			remain--
 		}
 	}
-	return len(runes)
-}
-
-func isSimpleHarf(harf rune) bool {
-	begin, end := 0, len(simpleHarfs)-1
-	for begin <= end {
-		mid := (begin + end) / 2
-		if harf > simpleHarfs[mid] {
-			begin = mid + 1
-		} else if harf < simpleHarfs[mid] {
-			end = mid - 1
-		} else {
-			return true
-		}
-	}
-	return false
+	return len(text) + 1
 }
