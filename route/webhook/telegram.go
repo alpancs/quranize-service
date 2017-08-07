e50aeb6c2c55a766b4f40b0ef49807ef9407a1c0
Alfan Nur Fauzan <alpancs@gmail.com>
2017-08-07 13:33:26 +0700

    try to response inline query

--- route/webhook/telegram.go
+++ route/webhook/telegram.go
@@ -1,12 +1,48 @@
 package webhook
 
 import (
-	"fmt"
-	"io/ioutil"
+	"bytes"
+	"encoding/json"
 	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/alpancs/quranize/service"
 )
 
+type Update struct {
+	InlineQuery `json:"inline_query"`
+}
+type InlineQuery struct{ Id, Query string }
+
+type InlineQueryAnswer struct {
+	InlineQueryId string              `json:"inline_query_id"`
+	Results       []InlineQueryResult `json:"results"`
+}
+type InlineQueryResult struct {
+	Type                string `json:"type"`
+	Id                  string `json:"id"`
+	Title               string `json:"title"`
+	InputMessageContent `json:"input_message_content"`
+}
+type InputMessageContent struct {
+	MessageText string `json:"message_text"`
+}
+
 func Telegram(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body)
-	fmt.Println(string(body))
+	var update Update
+	json.NewDecoder(r.Body).Decode(&update)
+	go reply(update.InlineQuery)
+}
+
+func reply(inlineQuery InlineQuery) {
+	telegramURL := "https://api.telegram.org/bot" + os.Getenv("QURANIZE_TELEGRAM_TOKEN") + "/answerInlineQuery"
+	results := []InlineQueryResult{}
+	for i, encoded := range service.Encode(inlineQuery.Query) {
+		id := strconv.Itoa(i)
+		results = append(results, InlineQueryResult{"article", id, encoded, InputMessageContent{encoded}})
+	}
+	answerBuffer := bytes.NewBuffer(nil)
+	json.NewEncoder(answerBuffer).Encode(InlineQueryAnswer{inlineQuery.Id, results})
+	http.Post(telegramURL, "application/json", answerBuffer)
 }
