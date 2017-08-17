a470d3c07e5dac5c70dfa0d778724e3b6aa280bd
Alfan Nur Fauzan <alpancs@gmail.com>
2017-08-17 21:10:13 +0700

    disable out of bound buttons

--- public/home.js
+++ public/home.js
@@ -2,6 +2,7 @@ let app = new Vue({
   el: '#app',
 
   data: {
+    ayaCount: [7,286,200,176,120,165,206,75,129,109,123,111,43,52,99,128,111,110,98,135,112,78,118,64,77,227,93,88,69,60,34,30,73,54,45,83,182,88,75,85,54,53,89,59,37,35,38,29,18,45,60,49,62,55,78,96,29,22,24,13,14,11,11,18,12,12,30,52,52,44,28,28,20,56,40,31,50,40,46,42,29,19,36,25,22,17,19,26,30,20,15,21,11,8,8,19,5,8,8,11,11,8,3,9,5,4,7,3,6,3,5,4,5,6],
     keyword: '',
     encodeds: [],
     loading: 0,
@@ -110,10 +111,10 @@ let app = new Vue({
         axios.get(`/api/aya/${location.suraNumber}/${location.ayaNumber+n}`)
         .then((response)=> ({beforeHighlightedAya: response.data}))
       ayaPromise = ayaPromise.then((aya) => {
-        this.$set(location, 'ayaNumber', location.ayaNumber+n)
         this.$set(location, 'beforeHighlightedAya', aya.beforeHighlightedAya)
         this.$set(location, 'highlightedAya', aya.highlightedAya)
         this.$set(location, 'afterHighlightedAya', aya.afterHighlightedAya)
+        this.$set(location, 'ayaNumber', location.ayaNumber+n)
       })
       .then(() => this.$set(location, 'loadingAya', false))
 
@@ -134,7 +135,7 @@ let app = new Vue({
       .then(() => this.$set(location, 'loadingTafsir', false))
 
       Promise.all([ayaPromise, translationPromise, tafsirPromise])
-      .catch((e) => e.response && e.response.status === 400 ? undefined : this.notify('connection problem'))
+      .catch(() => this.notify('connection problem'))
       .then(() => {
         this.$set(location, 'loadingAya', false)
         this.$set(location, 'loadingTranslation', false)
