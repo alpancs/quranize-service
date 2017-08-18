a53378045714b05c9596385d8c9fd3031d8bd607
Alfan Nur Fauzan <alpancs@gmail.com>
2017-08-18 11:43:55 +0700

    session storage

--- public/home.js
+++ public/home.js
@@ -38,6 +38,7 @@ let app = new Vue({
 
   methods: {
     updateResult: _.debounce(function() {
+      sessionStorage.setItem('keyword', this.trimmedKeyword)
       this.logged = false
       ++this.loading
       let currentRequestTime = Date.now()
