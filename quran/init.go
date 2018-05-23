18d2ead3f39cb53f01f60b787b5366e5088d9b3c
Alfan Nur Fauzan <alpancs@gmail.com>
2018-05-23 11:38:06 +0700

    refactor

--- quran/init.go
+++ quran/init.go
@@ -24,17 +24,6 @@ type Quran struct {
 	root *Node
 }
 
-func (q Quran) GetAya(sura, aya int) (string, error) {
-	if !(1 <= sura && sura <= len(q.Suras)) {
-		return "", errors.New(fmt.Sprintf("invalid sura number %d", sura))
-	}
-	ayas := q.Suras[sura-1].Ayas
-	if !(1 <= aya && aya <= len(ayas)) {
-		return "", errors.New(fmt.Sprintf("invalid sura number %d and aya number %d", sura, aya))
-	}
-	return ayas[aya-1].Text, nil
-}
-
 type Node struct {
 	Locations []Location
 	Children  []Child
@@ -58,10 +47,23 @@ var (
 	TranslationID       Quran
 	TafsirQuraishShihab Quran
 
-	transliteration Transliteration
+	transliteration = Transliteration{make(map[string][]string), 0}
 	emptyLocations  = make([]Location, 0, 0)
 	corpusPath      = getCorpusPath()
 )
+
+func init() {
+	startTime := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(5)
+	go loadTransliterationAsync(&wg, "arabic-to-alphabet", &transliteration)
+	go loadAndIndexQuranAsync(&wg, "quran-simple-clean.xml", &QuranClean)
+	go loadQuranAsync(&wg, "quran-simple-enhanced.xml", &QuranEnhanced)
+	go loadQuranAsync(&wg, "id.indonesian.xml", &TranslationID)
+	go loadQuranAsync(&wg, "id.muntakhab.xml", &TafsirQuraishShihab)
+	wg.Wait()
+	fmt.Println("service initialized in", time.Since(startTime))
+}
 
 func getCorpusPath() string {
 	if path := os.Getenv("CORPUS_PATH"); path != "" {
@@ -70,86 +72,26 @@ func getCorpusPath() string {
 	return "corpus/"
 }
 
-func init() {
-	startTime := time.Now()
-	var wg sync.WaitGroup
-	wg.Add(5)
-	go loadTransliterationAsync(&wg, "arabic-to-alphabet", &transliteration)
-	go loadQuranAndIndexAsync(&wg, "quran-simple-clean.xml", &QuranClean)
-	go loadQuranAsync(&wg, "quran-simple-enhanced.xml", &QuranEnhanced)
-	go loadQuranAsync(&wg, "id.indonesian.xml", &TranslationID)
-	go loadQuranAsync(&wg, "id.muntakhab.xml", &TafsirQuraishShihab)
-	wg.Wait()
-	fmt.Println("service initialized in", time.Since(startTime))
-}
-
-func loadTransliterationAsync(wg *sync.WaitGroup, fileName string, t *Transliteration) {
-	loadTransliteration(fileName, t)
-	wg.Done()
-}
-
-func loadTransliteration(fileName string, t *Transliteration) {
-	m := make(map[string][]string)
-	maxWidth := 0
-	raw, err := ioutil.ReadFile(corpusPath + fileName)
-	if err != nil {
-		panic(err)
+// Get aya text from sura number and aya number (number starting from 1)
+func (q Quran) GetAya(sura, aya int) (string, error) {
+	if !(1 <= sura && sura <= len(q.Suras)) {
+		return "", errors.New(fmt.Sprintf("invalid sura number %d", sura))
 	}
-	trimmed := strings.TrimSpace(string(raw))
-	for _, line := range strings.Split(trimmed, "\n") {
-		components := strings.Split(line, " ")
-		arabic := components[0]
-		for _, alphabet := range components[1:] {
-			m[alphabet] = append(m[alphabet], arabic)
-
-			length := len(alphabet)
-			ending := alphabet[length-1]
-			if ending == 'a' || ending == 'i' || ending == 'o' || ending == 'u' {
-				alphabet = alphabet[:length-1] + alphabet[:length-1] + alphabet[length-1:]
-			} else {
-				alphabet += alphabet
-			}
-			m[alphabet] = append(m[alphabet], arabic)
-			length = len(alphabet)
-			if length > maxWidth {
-				maxWidth = length
-			}
-		}
+	ayas := q.Suras[sura-1].Ayas
+	if !(1 <= aya && aya <= len(ayas)) {
+		return "", errors.New(fmt.Sprintf("invalid sura number %d and aya number %d", sura, aya))
 	}
-	t.Hijaiyas = m
-	t.MaxWidth = maxWidth
+	return ayas[aya-1].Text, nil
 }
 
-func loadQuranAsync(wg *sync.WaitGroup, fileName string, q *Quran) {
-	loadQuran(fileName, q)
-	wg.Done()
-}
-
-func loadQuranAndIndexAsync(wg *sync.WaitGroup, fileName string, q *Quran) {
-	loadQuran(fileName, q)
-	q.root = buildIndex(q)
-	wg.Done()
-}
-
-func loadQuran(fileName string, q *Quran) {
-	raw, err := ioutil.ReadFile(corpusPath + fileName)
-	if err != nil {
-		panic(err)
-	}
-	err = xml.Unmarshal(raw, q)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func buildIndex(q *Quran) *Node {
-	node := &Node{Locations: emptyLocations}
+// Build index of Quran q
+func (q *Quran) BuildIndex() {
+	q.root = &Node{Locations: emptyLocations}
 	for s, sura := range QuranClean.Suras {
 		for a, aya := range sura.Ayas {
-			indexAya([]rune(aya.Text), s, a, node)
+			indexAya([]rune(aya.Text), s, a, q.root)
 		}
 	}
-	return node
 }
 
 func indexAya(harfs []rune, sura, aya int, node *Node) {
@@ -183,4 +125,59 @@ func getChild(children []Child, key rune) *Node {
 		}
 	}
 	return nil
+}
+
+func loadTransliterationAsync(wg *sync.WaitGroup, fileName string, t *Transliteration) {
+	loadTransliteration(fileName, t)
+	wg.Done()
+}
+
+func loadTransliteration(fileName string, t *Transliteration) {
+	raw, err := ioutil.ReadFile(corpusPath + fileName)
+	if err != nil {
+		panic(err)
+	}
+	trimmed := strings.TrimSpace(string(raw))
+	for _, line := range strings.Split(trimmed, "\n") {
+		components := strings.Split(line, " ")
+		arabic := components[0]
+		for _, alphabet := range components[1:] {
+			t.Hijaiyas[alphabet] = append(t.Hijaiyas[alphabet], arabic)
+
+			length := len(alphabet)
+			ending := alphabet[length-1]
+			if ending == 'a' || ending == 'i' || ending == 'o' || ending == 'u' {
+				alphabet = alphabet[:length-1] + alphabet[:length-1] + alphabet[length-1:]
+			} else {
+				alphabet += alphabet
+			}
+			t.Hijaiyas[alphabet] = append(t.Hijaiyas[alphabet], arabic)
+			length = len(alphabet)
+			if length > t.MaxWidth {
+				t.MaxWidth = length
+			}
+		}
+	}
+}
+
+func loadQuranAsync(wg *sync.WaitGroup, fileName string, q *Quran) {
+	loadQuran(fileName, q)
+	wg.Done()
+}
+
+func loadAndIndexQuranAsync(wg *sync.WaitGroup, fileName string, q *Quran) {
+	loadQuran(fileName, q)
+	q.BuildIndex()
+	wg.Done()
+}
+
+func loadQuran(fileName string, q *Quran) {
+	raw, err := ioutil.ReadFile(corpusPath + fileName)
+	if err != nil {
+		panic(err)
+	}
+	err = xml.Unmarshal(raw, q)
+	if err != nil {
+		panic(err)
+	}
 }
