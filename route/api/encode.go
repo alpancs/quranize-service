df4cf99c276e420a14c27c4a9b76af24f3798340
Alfan Nur Fauzan <alpancs@gmail.com>
2018-05-26 18:37:37 +0700

    tell owner

--- route/api/encode.go
+++ route/api/encode.go
@@ -1,13 +1,39 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/alpancs/quranize/quran"
+)
+
+type Response struct {
+	ChatID string `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+var (
+	telegramAPI = fmt.Sprintf("https://api.telegram.org/bot%s/", os.Getenv("QURANIZE_TELEGRAM_TOKEN"))
 )
 
 func Encode(w http.ResponseWriter, r *http.Request) {
 	keyword := r.URL.Query().Get("keyword")
 	json.NewEncoder(w).Encode(quran.Encode(keyword))
+	go tellOwner(keyword)
+}
+
+func tellOwner(keyword string) {
+	reqBody, err := json.Marshal(Response{"@alpancs", keyword})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	url := telegramAPI + "sendMessage"
+	_, err = http.Post(url, "application/json", bytes.NewReader(reqBody))
+	if err != nil {
+		fmt.Println(err)
+	}
 }
