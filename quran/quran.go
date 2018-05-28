e5b5e78258f70e127ed5f8f684b8848e7d3c127d
Alfan Nur Fauzan <alpancs@gmail.com>
2018-05-28 14:39:33 +0700

    quran init time

--- quran/quran.go
+++ quran/quran.go
@@ -1,16 +1,56 @@
 package quran
 
 import (
+	"fmt"
+	"sync"
+	"time"
+
 	"github.com/alpancs/quranize"
 )
 
 var (
-	quran = quranize.NewDefaultQuranize()
+	q      quranize.Quranize
+	Encode func(string) []string
+	Locate func(string) []quranize.Location
 
-	Encode = quran.Encode
-	Locate = quran.Locate
+	QuranSimpleEnhanced quranize.Quran
+	TranslationID       quranize.Quran
+	TafsirID            quranize.Quran
 
-	QuranSimpleEnhanced = quranize.NewQuranSimpleEnhanced()
-	TranslationID       = quranize.NewIDIndonesian()
-	TafsirID            = quranize.NewIDMuntakhab()
+	wg sync.WaitGroup
 )
+
+func init() {
+	startTime := time.Now()
+
+	wg.Add(4)
+	go initQuranize()
+	go initQuranSimpleEnhanced()
+	go initTranslationID()
+	go initTafsirID()
+	wg.Wait()
+
+	fmt.Println("quran init time:", time.Since(startTime))
+}
+
+func initQuranize() {
+	q = quranize.NewDefaultQuranize()
+	Encode = q.Encode
+	Locate = q.Locate
+	wg.Done()
+}
+
+func initQuranSimpleEnhanced() {
+	QuranSimpleEnhanced = quranize.NewQuranSimpleEnhanced()
+	wg.Done()
+}
+
+func initTranslationID() {
+	TranslationID = quranize.NewIDIndonesian()
+	wg.Done()
+}
+
+func initTafsirID() {
+	TafsirID = quranize.NewIDMuntakhab()
+	wg.Done()
+}
