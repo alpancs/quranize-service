61d262dfb9daf8cb061034d1f0c3e0122a4e46fa
Alfan Nur Fauzan <alpancs@gmail.com>
2019-10-05 21:48:26 +0700

    Refactor (#7)
    * move transliteration and quran to watch
    
    * minor
    
    * delay in dev
    
    * explicit Cache-Control
    
    * bug fix
    
    * upgrade lodash
    
    * upgrade axios
    
    * upgrade vue
    
    * adjust delay
    
    * trim by vue
    
    * revert vue
    
    * disable recent keywords
    
    * utilize lodash and string interpolation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/alpancs/quranize-service/route"
 	"github.com/alpancs/quranize-service/route/api"
@@ -33,23 +34,31 @@ func newRouter() http.Handler {
 		router.Use(middleware.Logger)
 	}
 
-	router.Route("/", func(compressedRoute chi.Router) {
-		compressedRoute.Use(middleware.DefaultCompress)
-		compressedRoute.Use(header("Vary", "Accept-Encoding"))
+	router.Route("/", func(compressedRouter chi.Router) {
+		compressedRouter.Use(middleware.DefaultCompress)
+		compressedRouter.Use(header("Vary", "Accept-Encoding"))
 
-		homeRouter := compressedRoute.With(header("Content-Type", "text/html; charset=utf-8"))
+		homeRouter := compressedRouter.With(header("Content-Type", "text/html; charset=utf-8"))
 		homeRouter.Get("/", route.Home)
 		homeRouter.Get("/{keyword:^([A-Za-z' ]|%20)+$}", route.Home)
 
-		cachedRouter := compressedRoute.With(header("Cache-Control", "public, max-age=31536000"))
+		cacheControl := "no-store"
+		if isProduction {
+			cacheControl = "public, max-age=31536000"
+		}
+		cachedRouter := compressedRouter.With(header("Cache-Control", cacheControl))
 		fileServer(cachedRouter, "/", http.Dir("public"))
 	})
 
 	router.Route("/api", func(apiRouter chi.Router) {
-		apiRouter.Use(header("Content-Type", "application/json; charset=utf-8"))
-		cachedRouter := apiRouter.With(header("Cache-Control", "public, max-age=43200"))
 		if !isProduction {
-			cachedRouter = apiRouter
+			apiRouter.Use(delay(1200 * time.Millisecond))
+		}
+		apiRouter.Use(header("Content-Type", "application/json; charset=utf-8"))
+		apiRouter.Use(header("Cache-Control", "no-store"))
+		cachedRouter := apiRouter
+		if isProduction {
+			cachedRouter = apiRouter.With(header("Cache-Control", "public, max-age=43200"))
 		}
 		cachedRouter.Get("/encode", api.Encode)
 		cachedRouter.Get("/locate", api.Locate)
@@ -84,6 +93,15 @@ func header(key, value string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set(key, value)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func delay(duration time.Duration) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(duration)
 			next.ServeHTTP(w, r)
 		})
 	}
